1de39267ecab04375a3f28bbc383163520ff8139
mayowDev <dinnod4@gmail.com>
2020-01-20 05:26:18 +0500

    connected with react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>welcom To Three js with react</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
